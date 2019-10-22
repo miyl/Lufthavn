e217f012dbc2b49927e47cd47e6f77441cd010c2
Frederik Søndergaard Jensen <FSJ95@hotmail.dk>
2019-10-22 14:06:27 +0200

    Now extends DepartmentHandler

---
+++ control/src/main/java/dk/kea/handlers/DepartmentHandler.java
@@ -0,0 +1,51 @@
+package dk.kea.handlers;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class DepartmentHandler implements Runnable {
+
+    private Socket socket;
+    private DataInputStream input;
+    private DataOutputStream output;
+    private boolean isRunning = true;
+
+    public String name;
+
+    public DepartmentHandler(Socket socket, DataInputStream input, DataOutputStream output, String name) {
+        this.socket = socket;
+        this.input = input;
+        this.output = output;
+        this.name = name;
+
+
+        System.out.println(name + " department thread startet.");
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Dette er bare en test
+            while (isRunning) {
+
+                String message = input.readUTF();
+                output.writeUTF(message);
+                System.out.printf(name + " thread: %s \n", message );
+
+            }
+
+            input.close();
+            output.close();
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println("** " + name + " thread exited  **");
+
+            isRunning = false;
+        }
+    }
+}
+
--- control/src/main/java/dk/kea/handlers/FuelDepartmentHandler.java
+++ control/src/main/java/dk/kea/handlers/FuelDepartmentHandler.java
@@ -3,43 +3,10 @@ package dk.kea.handlers;
 import java.io.*;
 import java.net.Socket;
 
-public class FuelDepartmentHandler implements Runnable {
+public class FuelDepartmentHandler extends DepartmentHandler {
 
-    private Socket socket;
-    private DataInputStream input;
-    private DataOutputStream output;
-    private boolean isRunning = true;
+    public FuelDepartmentHandler(Socket socket, DataInputStream input, DataOutputStream output) {
+        super(socket, input, output, "Fuel");
 
-    public FuelDepartmentHandler(Socket socket, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
-        this.socket = socket;
-        this.input = dataInputStream;
-        this.output = dataOutputStream;
-
-
-        System.out.println("Fuel department thread startet.");
-
-    }
-
-    @Override
-    public void run() {
-        try {
-            // Dette er bare en test
-            while (isRunning) {
-
-                String message = input.readUTF();
-                output.writeUTF(message);
-                System.out.printf("Fuel thread: %s \n", message );
-
-            }
-
-            input.close();
-            output.close();
-            socket.close();
-
-        } catch (IOException e) {
-            System.out.println("** Fuel thread exited  **");
-
-            isRunning = false;
-        }
     }
 }
