d47c89a770dc920156f5b7eda43cda0ea0f40b33
Jarrald <novijgod@yahoo.dk>
2019-10-22 12:40:08 +0200

    Fælles maven project

---
+++ Lufthavns_System/src/main/java/dat18c/agiluge/App.java
@@ -0,0 +1,13 @@
+package dat18c.agiluge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Lufthavns_System/src/test/java/dat18c/agiluge/AppTest.java
@@ -0,0 +1,20 @@
+package dat18c.agiluge;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
