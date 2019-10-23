9ce8aceb81f8608ef3c27fdc739871db20f03292
Marcus Miyalys <resolutions@riseup.net>
2019-10-23 15:12:36 +0200

    JUNIT5 should be working

--- Client/src/test/java/dk/kea/AppTest.java
+++ Client/src/test/java/dk/kea/AppTest.java
@@ -1,37 +1,17 @@
 package dk.kea;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase
-{
+public class AppTest {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
+    @Test
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
     }
 }
