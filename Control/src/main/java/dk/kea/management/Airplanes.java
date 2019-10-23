1a26d22b68887a1340a1a5ca08ac4990640d5b7b
oliverlowbob <oliv605m@stud.kea.dk>
2019-10-23 15:00:54 +0200

    få fly ud fra databasen i airplanes

--- Control/src/main/java/dk/kea/management/Airplanes.java
+++ Control/src/main/java/dk/kea/management/Airplanes.java
@@ -1,6 +1,7 @@
 package dk.kea.management;
 
 import dk.kea.Models.Flights;
+import dk.kea.Models.Gate;
 import dk.kea.dbconnect.DBConnect;
 
 import java.sql.ResultSet;
@@ -17,7 +18,7 @@ public class Airplanes{
     }
 
 
-    public void getPlanes(){
+    public List getPlanes(){
         dbConnect = new DBConnect();
         List<Flights> flights = new ArrayList<Flights>();
         try {
@@ -25,7 +26,9 @@ public class Airplanes{
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM Fly");
             while(rs.next()){
+
                 Flights flight = new Flights();
+                Gate gate = new Gate();
                 flight.setId(rs.getInt("fly_id"));
                 flight.setName(rs.getString("name"));
                 flight.setModel(rs.getString("model"));
@@ -33,9 +36,18 @@ public class Airplanes{
                 flight.setLuftSelskab(rs.getString("luftSelskab"));
                 flight.setDeparture(rs.getDate("departure"));
                 flight.setArrival(rs.getDate("arrival"));
-                //flight.setGate(rs.getObject("gate"));
+                flight.setGate(gate);
                 flight.setPriorityNumber(rs.getInt("priorityNumber"));
                 flight.setStandPlads(rs.getInt("standPlads"));
+x
+                ResultSet rs_ = stmt.executeQuery("SELECT * FROM Gate WHERE id = " + flight.getId());
+                while(rs_.next()){
+                    gate.setNumber(rs_.getInt("number"));
+                    gate.setGateSize(rs_.getString("gateSize"));
+                    gate.setTerminal(rs_.getString("terminal"));
+                }
+
+
                 flights.add(flight);
 
 
@@ -44,6 +56,7 @@ public class Airplanes{
         }catch (SQLException e){
             e.printStackTrace();
         }
+        return flights;
     }
 
 
