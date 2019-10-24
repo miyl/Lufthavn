179a3d410776fd27aa54525a747f8a33f98aa870
oliverlowbob <oliv605m@stud.kea.dk>
2019-10-24 10:14:54 +0200

    flightdbhandler med metode+gatedbhandler med getmetode

---
+++ Control/src/main/java/dk/kea/management/GateDbHandler.java
@@ -0,0 +1,38 @@
+package dk.kea.management;
+
+import dk.kea.dbconnect.DBConnect;
+import dk.kea.models.Gate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GateDbHandler {
+    DBConnect dbConnect;
+
+    public List<Gate> getGates(){
+        dbConnect = new DBConnect();
+        List<Gate> gates = new ArrayList<Gate>();
+
+        try {
+            Statement stmt = dbConnect.getConnection().createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Gates");
+            while(rs.next()){
+
+                Gate gate = new Gate();
+                gate.setNumber(rs.getInt("number"));
+                gate.setGateSize(rs.getString("gateSize"));
+                gate.setTerminal(rs.getString("terminal"));
+
+                gates.add(gate);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return gates;
+    }
+}
