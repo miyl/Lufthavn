012ce5ce3893d70579a5ff2c4748e4ea29fa113f
Jarrald <novijgod@yahoo.dk>
2019-10-25 09:52:48 +0200

    Update FuelHandler.java

--- Client/src/main/java/dk/kea/departments/FuelHandler.java
+++ Client/src/main/java/dk/kea/departments/FuelHandler.java
@@ -19,80 +19,103 @@ public class FuelHandler extends ServerHandler {
     public void start(){
 
         while (isConnected()) {
+            //Får information fra keyboardet - hvis der er noget.
+            System.out.printf("> ");
+            if (keyboard.getReader().hasNextLine()) {
+                String[] tokens = keyboard.getReader().nextLine().toUpperCase().split(" ");
 
-            if (getFlightList().size()>0)
-            {
-                for (Flight flight : getFlightList())
+                switch(tokens[0])
                 {
-                    long currentTime = flight.getExpectedDeparture().getTime();
+                    case "EXIT":
+                        close();
+                        break;
+                    case "LIST":
+                        if(getFlightList().size() > 0)
+                        {
+                            System.out.print("[INFO]: Active planes in this department:\n\n");
 
-                    if (flight.getFlightSize().equalsIgnoreCase("lille"));
-                    {
-                        flight.setExpectedDeparture(new Timestamp(currentTime / Time.milliseconds + Time.fuelLille));
+                            getFlightList().forEach(plane -> System.out.print("      [" + plane.getName() + "]\n"));
+
+                            System.out.println();
+                        } else {
+                            System.out.print("[INFO]: No active planes in this department.\n");
+                        }
+                        break;
+                    case "SEND":
+                        if(getFlightList().size() > 0){
+                            sender.sendPlanes(getFlightList());
+                            System.out.println("[INFO]: Flights is send to server");
+                        } else {
+                            System.out.print("[ERROR]: No active planes in this apartment.\n");
+                        }
+                        break;
+                    case "MANI":
+                        manipulate();
+                        break;
+                    case "REMOVE":
+                        if(getFlightList().size() > 0)
+                        {
+                            getFlightList().forEach(plane -> System.out.print("      [" + plane.getName() + "]\n"));
+                            System.out.println("      removed from local list.");
+                            removeFlightList();
+                        } else {
+                            System.out.print("[INFO]: No active planes in this department.\n");
+                        }
+
+                        break;
+                    default:
+                        sender.send(String.join(" ", tokens), false);
+                        break;
+                }
+            }
+        }
+    }
+    public void manipulate(){
+
+        if (getFlightList().size() > 0) {
+            getFlightList().forEach(flight -> {
+                        long currentTime = flight.getExpectedDeparture().getTime();
+                        if (flight.getFlightSize().equalsIgnoreCase("LILLE")) {
+                            System.out.println("Updating: " + flight.getId());
+                            flight.setExpectedDeparture(new Timestamp(currentTime + Time.milliseconds * Time.fuelLille));
+                        }
+                        else if(flight.getFlightSize().equalsIgnoreCase("MELLEM")){
+                            System.out.println("Updating: " + flight.getId());
+                            flight.setExpectedDeparture(new Timestamp(currentTime + Time.milliseconds * Time.fuelMellem));
+                        }
+                        else{
+                            System.out.println("Updating: " + flight.getId());
+                            flight.setExpectedDeparture(new Timestamp(currentTime + Time.milliseconds * Time.fuelStor));
+                        }
                     }
+            );
+            System.out.println("[INFO] Flight manipulated.");
+        }
+        /*
+        if (getFlightList().size()>0)
+        {
+            for (Flight flight : getFlightList())
+            {
+                long currentTime = flight.getExpectedDeparture().getTime();
 
-                    if (flight.getFlightSize().equalsIgnoreCase("mellem"));
-                    {
-                        flight.setExpectedDeparture(new Timestamp(currentTime / Time.milliseconds + Time.fuelMellem));
-                    }
-
-                    if (flight.getFlightSize().equalsIgnoreCase("stor"));
-                    {
-                        flight.setExpectedDeparture(new Timestamp(currentTime / Time.milliseconds + Time.fuelStor));
-                    }
-
+                if (flight.getFlightSize().equalsIgnoreCase("lille"));
+                {
+                    flight.setExpectedDeparture(new Timestamp(currentTime / Time.milliseconds + Time.fuelLille));
                 }
 
+                if (flight.getFlightSize().equalsIgnoreCase("mellem"));
+                {
+                    flight.setExpectedDeparture(new Timestamp(currentTime / Time.milliseconds + Time.fuelMellem));
+                }
 
+                if (flight.getFlightSize().equalsIgnoreCase("stor"));
+                {
+                    flight.setExpectedDeparture(new Timestamp(currentTime / Time.milliseconds + Time.fuelStor));
+                }
 
-                sender.sendPlanes(getFlightList());
             }
-            //Får information fra keyboardet - hvis der er noget.
-//            System.out.printf("> ");
-//            if (keyboard.getReader().hasNextLine()) {
-//                String[] tokens = keyboard.getReader().nextLine().toUpperCase().split(" ");
-//
-//                switch(tokens[0])
-//                {
-//                    case "EXIT":
-//                        close();
-//                        break;
-//                    case "LIST":
-//                        if(getFlightList().size() > 0)
-//                        {
-//                            System.out.print("[INFO]: Active planes in this department:\n\n");
-//
-//                            getFlightList().forEach(plane -> System.out.print("      [" + plane.getName() + "]\n"));
-//
-//                            System.out.println();
-//                        } else {
-//                            System.out.print("[INFO]: No active planes in this department.\n");
-//                        }
-//                        break;
-//                    case "SEND":
-//                        if(getFlightList().size() > 0){
-//                            sender.sendPlanes(getFlightList());
-//                            System.out.println("[INFO]: Flights is send to server");
-//                        } else {
-//                            System.out.print("[ERROR]: No active planes in this apartment.\n");
-//                        }
-//                        break;
-//                    case "REMOVE":
-//                        if(getFlightList().size() > 0)
-//                        {
-//                            getFlightList().forEach(plane -> System.out.print("      [" + plane.getName() + "]\n"));
-//                            System.out.println("      removed from local list.");
-//                            removeFlightList();
-//                        } else {
-//                            System.out.print("[INFO]: No active planes in this department.\n");
-//                        }
-//
-//                        break;
-//                    default:
-//                        sender.send(String.join(" ", tokens), false);
-//                        break;
-//                }
-//            }
         }
+
+         */
     }
 }
